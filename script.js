562ff115c47cbc92e677fcbc32813209c9e5b76c
vipin singh rawat <rawatvipin756@gmail.com>
2026-07-24 18:16:24 +0530

    add of delete button

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let button=document.getElementById("addBtn");
 let list=document.getElementById("taskList");
 
 button.addEventListener("click",function () {
-    let li=document.createElement("li");
-    li.innerText=input.value;
-    list.appendChild(li);
-    input.value="";
+    if (input.value.trim() != "") {
+        let li = document.createElement("li");
+        li.innerText = input.value.trim();
+        list.appendChild(li);
+        input.value = "";
+    }
 });
