3615837b0e0ec8849c410376c3f9fb5f11f09ca3
vipin singh rawat <rawatvipin756@gmail.com>
2026-07-26 12:38:12 +0530

    addition of new features

--- script.js
+++ script.js
@@ -2,11 +2,31 @@ let input=document.getElementById("taskInput");
 let button=document.getElementById("addBtn");
 let list=document.getElementById("taskList");
 
-button.addEventListener("click",function () {
+function addTask(){
     if (input.value.trim() != "") {
         let li = document.createElement("li");
         li.innerText = input.value.trim();
         list.appendChild(li);
+        let deleteBtn=document.createElement("button");
+        deleteBtn.innerText = "Delete";
+        li.appendChild(deleteBtn);
+        deleteBtn.addEventListener("click",function() {
+            li.remove();
+        })
         input.value = "";
     }
+};
+
+button.addEventListener("click",function () {
+    addTask();
+});
+
+input.addEventListener("keydown",function(event) {
+    if(event.key==="Enter"){
+        addTask();
+    }
+});
+
+list.addEventListener("click",function() {
+    li.style.textDecoration = "line-through";
 });
